f686395336409c2b774c943f0eb001ea550ab00d
Joasia Klasik <asklas@op.pl>
2025-11-19 19:24:14 +0100

    Upgrate to using time vector

--- main.m
+++ main.m
@@ -3,13 +3,16 @@ A = 1;
 % Initial condition
 phi0 = 1;
 % Time
-t = 2;
+t = [0:2];
 % Force as a function
 f = @(s) (s < 0).*0 + (s >= 0).*1;
 
 
 % Solution
-x = solution(A, phi0, f, t)
+x = solution(A, phi0, f, t);
 
+plot(t, x)
+xlabel("time")
+ylabel("x(t)")
 
 
--- solution.m
+++ solution.m
@@ -1,4 +1,25 @@
-function x = solution(A, phi0, f, t)
-    integrandFunction = @(s) expm(A * (t-s)) * f(s);
-    x = expm(A*t)*phi0 + integral(integrandFunction,0,t,'ArrayValued',true);
+function X = solution(A, phi0, f, t)
+% A      : n×n matrix
+% phi0   : n×1 initial state
+% f      : input function
+% t      : vector of times 1×m or mx1
+% X      : solution - n×m matrix, each column is x(t_k)
+
+    phi0 = phi0(:); % Ensure phi0 is a column vector
+    t = t(:).';
+    
+    if size(A, 1) ~= size(phi0,1)
+        error("Incorrect dimentions for phi0 and A ")
+    end
+
+    m = numel(t);
+    n = size(A, 1);
+    X = zeros(n, m);
+
+    for k = 1:m
+        tk = t(k);
+        integrandFunction = @(s) expm( A * (tk-s)) * f(s);
+        I = integral(integrandFunction, 0, tk, 'ArrayValued', true);
+        X(:,k) = expm(A * tk) * phi0 + I; 
+    end
 end
