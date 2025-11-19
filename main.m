44d1d914fde7940754b3e569bc67608672590642
Joasia Klasik <asklas@op.pl>
2025-11-19 20:33:43 +0100

    Allow for many dimentional states and inputs

--- main.m
+++ main.m
@@ -1,15 +1,15 @@
 % Model parameters
-A = 1;
+A = [1, 0; 0, 2];
 % Initial condition
-phi0 = 1;
+phi0 = [1; 1];
 % Time
 t = [0:2];
 % Force as a function
 f = @(s) (s < 0).*0 + (s >= 0).*1;
-
+fvec = @(s) [f(s); f(s)];
 
 % Solution
-x = solution(A, phi0, f, t);
+x = solution(A, phi0, fvec, t);
 
 plot(t, x)
 xlabel("time")
--- solution.m
+++ solution.m
@@ -1,15 +1,15 @@
 function X = solution(A, phi0, f, t)
 % A      : n×n matrix
 % phi0   : n×1 initial state
-% f      : input function
+% f      : function handle f(s) -> n×1
 % t      : vector of times 1×m or mx1
-% X      : solution - n×m matrix, each column is x(t_k)
+% X      : solution -> n×m matrix, each column is x(t_k)
 
     phi0 = phi0(:); % Ensure phi0 is a column vector
-    t = t(:).';
+    t = t(:).'; % Ensure t is a row vector
     
     if size(A, 1) ~= size(phi0,1)
-        error("Incorrect dimentions for phi0 and A ")
+        error("Incorrect dimentions for phi0, A or f")
     end
 
     m = numel(t);
