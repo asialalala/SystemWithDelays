1fed9d29d2c12f72bf72fd0216b70082ac0e7332
Joasia Klasik <asklas@op.pl>
2026-05-07 21:44:54 +0200

    Correct Lambert W

--- Lambert/lambertSolver.m
+++ Lambert/lambertSolver.m
@@ -1,53 +1,45 @@
 % Lambert Function W
+A = 3;
+B = 2;
+C_val = 1;
+
+h = 0.1;
+tk = 10;
+t_span = 0:h:tk;
+
+% 2. Problem definition
+phi = exampleRddeFunctions.get_phi1(C_val);
+f_sol = exampleRddeFunctions.get_sol1(A, B, C_val);
+
+x_exact = f_sol(t_span); % Reference
 
 % Model parameters dy(t)+alfa*y(t-T)+beta(t)=0, T>0
-alfa = 1;
-beta = 2;
-T = 1;
-phi = @(teta) -exp(teta); % initial 
+alfa = -A;
+beta = 0;
+T = B;
 
 N = 1000; % Lambert modes used in the method
 
-s = @(k) (1/T * lambertw(k,-alfa*T*exp(beta*T)) - beta);
-epsilon = @(k,t) exp(s(k)*t);
-
 k_vec = -N:N;
-% [0, N] divided on 2N elements
-t_vec = linspace(T, 0, 2*N + 1)';
+s_vec = (1/T .* lambertw(k_vec, -alfa * T * exp(beta * T)) - beta);
+t_vec = linspace(T, 0, 2*N + 1)' - T; % [0, N] divided on 2N elements
 
 % Omega initialization
-Omega2 = zeros(2*N + 1, 2*N + 1);
-for i = 1:length(t_vec)
-    for j = 1:length(k_vec)
-        Omega2(i,j) = epsilon(k_vec(j), t_vec(i));
-    end
-end
+[S_grid, T_grid] = meshgrid(s_vec, t_vec);
+Omega = exp(S_grid .* T_grid);
 
-[K, T_grid] = meshgrid(k_vec, t_vec);
-S_vals = arrayfun(s, K);
-Omega = exp(S_vals .* T_grid);
+% Coefficients C
+Phi = phi(t_vec);
+C = Omega \ Phi;
 
-
-Phi = phi(t_vec-T);
-
-C = Omega\Phi;
-
-% Calculate the response for each branch k
-syms t_sym real
-x_sym = 0;
-for idx = 1:length(k_vec)
-    Sk = s(k_vec(idx)); 
-    x_sym = x_sym + C(idx) * exp(Sk * t_sym); 
-end
-
-% disp('Analityczny wzór na x(t):');
-% pretty(vpa(x_sym, 4))
-
-t_plot = linspace(0, 5, 100);
-x_numeric = double(subs(x_sym, t_sym, t_plot));
+[S_final, T_final] = meshgrid(s_vec, t_span);
+x_matrix = exp(S_final .* T_final) .* C.'; 
+x_numeric = sum(x_matrix, 2);
 
 figure;
-plot(t_plot, real(x_numeric), 'LineWidth', 2);
+plot(t_span, x_exact, 'r', 'LineWidth', 2); hold on;
+plot(t_span, real(x_numeric), 'b--', 'LineWidth', 1.5);
 grid on;
-title('Rozwiązanie x(t) wyznaczone metodą Lamberta W');
-xlabel('t'); ylabel('x(t)');
+legend('Dokładne', 'Lambert W');
+title('Rozwiązanie DDE metodą Lamberta W');
+
